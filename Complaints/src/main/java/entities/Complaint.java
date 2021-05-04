ccd12dc76167dcf639dd14370cb2ffc75ca9fd3b
Anna Lehnhardt <anula.le@wp.pl>
2021-05-04 22:01:54 +0200

    filter

--- Complaints/src/main/java/entities/Complaint.java
+++ Complaints/src/main/java/entities/Complaint.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -19,6 +20,9 @@ import javax.validation.constraints.Size;
  * @author anna
  */
 @Entity
+@NamedQuery(name = "Complaint.findByStatus", query =
+"SELECT c FROM Complaint c WHERE c.status = :status"
+)
 public class Complaint implements Serializable {
 
     private static final long serialVersionUID = 1L;
