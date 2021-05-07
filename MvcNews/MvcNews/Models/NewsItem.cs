52d51eb2d8eeb42fa44dff83552197b67563089f
Anna Lehnhardt <anula.le@wp.pl>
2021-05-07 20:26:50 +0200

    MvcNews

---
+++ MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcNews.Models
+{
+	public class NewsItem
+	{
+		[Key]
+		public int Id { get; set; }
+		
+		[DataType(DataType.Date)]
+		public DateTime TimeStamp { get; set; }
+
+		[Required(ErrorMessage = "Text is required")]
+		[StringLength(140, ErrorMessage = "Text must be between 5 and 140 characters", MinimumLength =5)]
+		public string Text { get; set; }
+
+	}
+}
